320115a85fc2d2562fb86e25a23c29162a0106a0
Ikuo Terado <eqobar@gmail.com>
2016-08-29 06:03:27 +0900

    Add example

---
+++ sample/src/main/java/com/eqot/fiberscope/Example.java
@@ -0,0 +1,7 @@
+package com.eqot.fiberscope;
+
+public class Example {
+    private int add(int value1, int value2) {
+        return value1 + value2;
+    }
+}
--- sample/src/main/java/com/eqot/fiberscope/MainActivity.java
+++ sample/src/main/java/com/eqot/fiberscope/MainActivity.java
@@ -1,32 +1,25 @@
 package com.eqot.fiberscope;
 
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
-import com.eqot.fiberscope.processor.Accessible;
-import com.stablekernel.annotationprocessor.generated.GeneratedClass;
-
-@Accessible
 public class MainActivity extends AppCompatActivity {
-
-    @Accessible
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        showAnnotationMessage();
+//        showAnnotationMessage();
     }
 
-    private void showAnnotationMessage() {
-        GeneratedClass generatedClass = new GeneratedClass();
-        String message = generatedClass.getMessage();
-
-        new AlertDialog.Builder(this)
-                .setPositiveButton("Ok", null)
-                .setTitle("Annotation Processor Messages")
-                .setMessage(message)
-                .show();
-    }
+//    private void showAnnotationMessage() {
+//        GeneratedClass generatedClass = new GeneratedClass();
+//        String message = generatedClass.getMessage();
+//
+//        new AlertDialog.Builder(this)
+//                .setPositiveButton("Ok", null)
+//                .setTitle("Annotation Processor Messages")
+//                .setMessage(message)
+//                .show();
+//    }
 }
